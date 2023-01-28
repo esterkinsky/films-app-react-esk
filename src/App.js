2d950621a93e7c7f6b3961f350fd148ee8183137
ester kinsky <esterkinsky@mail.ru>
2023-01-28 04:16:12 +0300

    finally without type-search

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Header} from './components/layout/Header.jsx';
+import { Footer } from './components/layout/Footer.jsx';
+import {Main} from './components/Main.jsx'
+
+function App() {
+  return (
+  <>
+  <Header/>
+  <Main/>
+  <Footer/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return <nav className="purple darken-3">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">films-app</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+      </ul>
+    </div>
+  </nav>
+};
+
+export {Header};
